9f8f3e74d454782d8603310b4fcedb942ec036ec
guidou3 <guido.santi@studenti.unipd.it>
2016-08-02 15:46:46 +0200

    Tutti gli action creators (di cui molti vuoti)

---
+++ modules/actions/Da fare/Da sistemare/changeDSLIPermits.js
@@ -0,0 +1,34 @@
+export function requestChangeDSLIPermits() {
+	return { type: waitingChangeDSLIPermits }
+}
+
+export function receiveChangeDSLIPermits(bool, text) {
+	if(bool) return { 
+		type: successChangeDSLIPermits,
+		newLevel: text
+	}
+	else return { 
+		type: failedChangeDSLIPermits,
+		error: text
+		}
+}
+
+export function changeDSLIPermits(newLevel) {
+	
+	store.dispatch(requestChangeDSLIPermits())
+	return
+	{
+		request
+		.put('url')
+		.send({
+			level: newLevel
+		})
+		.then(function(err) {
+				store.dispatch(receiveChangeDSLIPermits(false, err)
+			},
+			function(){
+				store.dispatch(receiveChangeDSLIPermits(true, newLevel) //il reducer deve modificare state.currentDSLI.level
+			}
+		)
+	}
+}
