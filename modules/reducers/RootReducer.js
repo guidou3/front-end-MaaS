e510d4c701baa7b7207d88e583029cc58a251605
rdamico0 <roberto.damico.1@studenti.unipd.it>
2016-08-26 10:09:19 +0200

    fixed thing with reducers

--- modules/reducers/RootReducer.js
+++ modules/reducers/RootReducer.js
@@ -17,47 +17,5 @@ export default combineReducers({
 	currentDSLI: currentDSLIReducer,
 	currentUser: currentUserReducer,
 	status: statusReducer,
-		companies : companiesReducer,
-		auth : authReducer,
-		sys : systemReducer,
-		routing: routerReducer,
-		profile: infoReducer,
-	})
-
-export default function authReducer(state = 0, action) {
-	switch (action.type) {
-    case 'AT':
-      return 1
-		case 'AL':
-		  return 0
-    default:
-      return state
-  }
-}
-
-export default function systemReducer(state = '', action) {
-	switch (action.type) {
-    case 'ERR':
-      return action.err
-    default:
-      return ''
-  }
-}
-
-export default function infoReducer(state = '', action) {
-		switch (action.type) {
-	    case 'REQUESTED_PROFILE':
-	      return action.value
-	    default:
-	      return state
-	  }
-}
-
-export default function companiesReducer(state = 0, action) {
-	switch (action.type) {
-    case 'RECEIVED_COMPANIES':
-      return action.companies
-    default:
-      return state
-  }
-}
+	routing: routerReducer
+})
