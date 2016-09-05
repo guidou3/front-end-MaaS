bab0418335e694740f610522d079041f6b8b5d5a
sebastiano <zamb24@gmail.com>
2016-09-05 14:17:15 +0200

    correzione DocumentModel

--- modules/model/DocumentModel.js
+++ modules/model/DocumentModel.js
@@ -1,58 +1,78 @@
 /*jshint esversion: 6 */
 /*
-* Name : dashboard.js
-* Module : Back-end::lib::Model::dashboard
-* Location : /model/
-*
-* History :
-*
-* Version         Date           Programmer
-* =================================================
-* 0.0.1           2016-08-13     Berselli Marco
-* -------------------------------------------------
-* Codifica modulo
-* =================================================
-*/
+ * Name : dashboard.js
+ * Location : /model/
+ *
+ * History :
+ *
+ * Version         Date           Programmer
+ * =================================================
+ * 0.0.1           2016-08-13     Berselli Marco
+ * 0.1.0           2016-08-18    Zamberlan Sebastiano
+ * -------------------------------------------------
+ * Codifica modulo
+ * =================================================
+ */
 
- //import React, { Component, PropTypes } from 'react'
+//import React, { Component, PropTypes } from 'react'
 var AttributeReader = require('../utils/AttributeReader');
 
-class DocumentModel /*extends Component*/{
-  constructor(params, populate, bodyRows){
-   //super()
-   AttributeReader.assertEmptyAttributes(params,function(param){});//da inserire l'errore
-   AttributeReader.readRequiredAttributes(params,this,["collection","name"],function(param){});//lancio errore
-   this.populate = [];
-   AttributeReader.readOptionalAttributes(populate,this,["populate"]);
-   this.rows = bodyRows;
-}
-//Manca la parte di join
-buildQuery(){
-  return "db.collection(" + this.collection + ").find({label:" + this.name + "})";
-}
+class DocumentModel /*extends Component*/
+{
+  constructor(params, populate, bodyRows)
+  {
+    //super()
+    AttributeReader.assertEmptyAttributes(params, function(param) {}); //da inserire l'errore
+    AttributeReader.readRequiredAttributes(params, this, [
+      "collection", "name"
+    ], function(param) {}); //lancio errore
+    this.populate = [];
+    AttributeReader.readOptionalAttributes(populate, this, [
+      "populate"
+    ]);
+    this.rows = bodyRows;
+  }
+  buildQuery()
+  {
+    return "db.collection(" + this.collection + ").find()";
+  }
 
-getCollection(){
-  return this.collection;
-}
-getName(){
- return this.name;
-}
-getPopulate(){
-  return this.populate;
-}
-getRows(){
-  return this.rows;
-}
+  getCollection()
+  {
+    return this.collection;
+  }
+  getName()
+  {
+    return this.name;
+  }
+  getPopulate()
+  {
+    return this.populate;
+  }
+  getRows()
+  {
+    return this.rows;
+  }
 
-DSLType(){
-       return "document";
-}
-JSONbuild(){
-  return {
-    "proprierties":{"DSLType": this.DSLType(), "rows":this.rows},
-    "data":{}
-  };
-}
+  DSLType()
+  {
+    return "document";
+  }
+  JSONbuild(
+    return)
+  {
+    return {
+      "proprierties":
+      {
+        "DSLType": this.DSLType(),
+        "rows": this.rows
+      },
+      "data":
+      {
+        "return": return
+      }
+    };
+  }
 };
 //export default DocumentModel
 module.exports = DocumentModel
