bf7e4bcb11a45b77463afd73a95f99d7c9b62fff
sebastiano <zamb24@gmail.com>
2016-09-07 22:17:04 +0200

    aggiornamento, correzione e inserimento errori in DocumentModel.js

--- modules/model/DocumentModel.js
+++ modules/model/DocumentModel.js
@@ -1,6 +1,7 @@
 /*jshint esversion: 6 */
 /*
  * Name : dashboard.js
+ * FrontEnd::Model::DocumentModel
  * Location : /model/
  *
  * History :
@@ -9,52 +10,86 @@
  * =================================================
  * 0.0.1           2016-08-13     Berselli Marco
  * 0.1.0           2016-08-18    Zamberlan Sebastiano
+ * 0.1.1           2016-08-27    Zamberlan Sebastiano
  * -------------------------------------------------
  * Codifica modulo
  * =================================================
  */
-
 import React, { Component, PropTypes } from 'react'
+import request from 'superagent'
 var AttributeReader = require('../utils/AttributeReader');
+var MaasError = require("../utils/MaasError");
 
-class DocumentModel /*extends Component*/
-{
-  constructor(params, populate, bodyRows)
-  {
-    //super()
-    AttributeReader.assertEmptyAttributes(params, function(param) {}); //da inserire l'errore
+class DocumentModel extends Component{
+  constructor(params, populate, bodyRows){
+    super()
+    var self = this;
+
+    //Lettura Attributi Obbligatori
     AttributeReader.readRequiredAttributes(params, this, [
-      "collection", "name"
-    ], function(param) {}); //lancio errore
+      "collection"
+    ], function(param) {
+      throw new MaasError(8000,
+        "Required parameter '" + param + "' in document '" +
+          self.toString() + "'");
+    });
+
     this.query = "{}";
-    AttributeReader.readOptionalAttributes(params, this, [
-        "query"
-      ], function(param) {}) // lancio errore
+
+    //Lettura Attributi Opzionali
+    AttributeReader.readOptionalAttributes(params, this, ["query"]);
+
     this.populate = [];
+
+    //Lettura Attributi Opzionali all'interno di populate
     AttributeReader.readOptionalAttributes(populate, this, [
-      "populate"
-    ]);
-    this.rows = bodyRows;
+      "populate"]);
+
+    //Assegnazione delle righe all'interno del parametro rows
+    //this.rows = bodyRows;
+    for(var i=0; i<bodyRows.length; i++)
+    {
+      AttributeReader.readRequiredAttributes(bodyRows[i],this.rows,
+        ["label","name"], function(param){
+        throw new MaasError(8000,
+          "Required parameter '" + param + "' in document.rows '" +
+            self.toString() + "'");
+      });
+    }
+
+    //Lettura Attributi con Valore Vuoto
+    AttributeReader.assertEmptyAttributes(params, function(param) {
+      throw new MaasError(8000,
+      "Unexpected parameter '" + param + "' in document '"
+      + self.toString() + "'");
+    });
   }
 
+  //Metodi della Classe
+
+  //Metodo Creazione Query
   buildQuery()
   {
-    return "db.collection('" + this.collection + "').find(" + this.query +
-      ")";
+    return "db.collection('" + this.collection + "').find("
+     + this.query +")";
   }
 
+  //Metodi get
   getCollection()
   {
     return this.collection;
   }
+
   getName()
   {
     return this.name;
   }
+
   getPopulate()
   {
     return this.populate;
   }
+
   getRows()
   {
     return this.rows;
@@ -64,6 +99,8 @@ class DocumentModel /*extends Component*/
   {
     return "document";
   }
+
+  //Metodo
   JSONbuild(result)
   {
     return {
@@ -79,5 +116,6 @@ class DocumentModel /*extends Component*/
     };
   }
 };
-//export default DocumentModel
-module.exports = DocumentModel
+
+export default DocumentModel
+//module.exports = DocumentModel
