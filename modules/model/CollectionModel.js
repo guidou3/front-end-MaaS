3a0cab65c432f4f309afb26c14ff92bd707bbf6f
sebastiano <zamb24@gmail.com>
2016-09-10 10:46:59 +0200

    aggiornamento con gli errori del collection model

--- modules/model/CollectionModel.js
+++ modules/model/CollectionModel.js
@@ -1,82 +1,149 @@
+/*jshint esversion: 6 */
+/*
+ * Name : dashboard.js
+ * FrontEnd::Model::DashboardModel
+ * Location : /model/
+ *
+ * History :
+ *
+ * Version         Date           Programmer
+ * =================================================
+ * 0.0.1           2016-08-13    Zamberlan Sebastiano
+ * 0.1.0           2016-08-18    Berselli Marco
+ * 0.1.1           2016-08-27    Berselli Marco
+ * 0.1.2           2016-08-30    Zamberlan Sebastiano
+ * 0.1.3           2016-09-02    Zamberlan Sebastiano
+ * -------------------------------------------------
+ * Codifica modulo
+ * =================================================
+ */
+import MaasError from "../utils/MaasError";
 import AttributeReader from '../utils/AttributeReader'
 import {executeQuery} from '../utils/DSLICompiler'
 import * as actions from '../actions/RootAction'
 import React from 'react'
 
+
 class CollectionModel {
   constructor(params, index, show){
-    //super()
-    AttributeReader.assertEmptyAttributes(params,function(param){});//lancio dell'errore
-    AttributeReader.readRequiredAttributes(params,this,["param"],function(param){});//lancio dell'errore
-    AttributeReader.readRequiredAttributes(this.param,this,["name"],function(param){});//lancio dell'errore
-    AttributeReader.assertEmptyAttributes(index,function(param){});//lancio dell'errore
+    var self = this;
+
     //Index
     this.param = [];
     this.columns = [];
 
-    AttributeReader.readOptionalAttributes(index,this,["param","columns"]);
+    //Lettura Attributi Obbligatori
+    AttributeReader.readRequiredAttributes(params,this,[
+      "param"],function(param){
+        throw new MaasError(8000,
+          "Required parameter '" + param + "' in collection '" +
+            self.toString() + "'");
+      });
+
+    //Lettura Attributi con Valore Vuoto
+    AttributeReader.assertEmptyAttributes(params,function(param){
+      throw new MaasError(8000,
+      "Unexpected parameter '" + param + "' in collection '"
+      + self.toString() + "'");
+      });
+
+    //Lettura Attributi Obbligatori dentro l'attributo param
+    AttributeReader.readRequiredAttributes(this.param,this,[
+      "name"],function(param){
+        throw new MaasError(8000,
+        "Required parameter '" + param + "' in collection '" +
+          self.toString() + "'");
+      });
+
     this.populate = [];
     this.sortby = "{'_id': 1}";
     this.order = "asc";
 
-    AttributeReader.readOptionalAttributes(this.param,this,["populate","sortby","order","query"]);
+    //Lettura Attributi Opzionali dentro l'attributo param
+    AttributeReader.readOptionalAttributes(this.param,this,[
+      "populate","sortby","order","query"]);
+
     this.indexPopulate = this.populate;
+
+    //Lettura Attributi Opzionali dentro index
+    AttributeReader.readOptionalAttributes(index,this,[
+      "param","columns"]);
+
+    //Lettura Attributi con Valore Vuoto  dentro index
+    AttributeReader.assertEmptyAttributes(index,function(param){
+      throw new MaasError(8000,
+      "Unexpected parameter '" + param + "' in collection.index '"
+      + self.toString() + "'");
+    });
+
     //Show
-
-    AttributeReader.readOptionalAttributes(show,this,["populate","rows"]);
-
-    this.flag = true;
-    this.show = false ;
-    this.storageResult = [];
-    this.secondQuery = [];
-    this.count =0;
-    this.JSON;
+    AttributeReader.readOptionalAttributes(show,this,[
+      "populate","rows"]);
   }
 
   getName(){
     return this.name;
   }
+
   getIndexColumns(){
     return this.columns;
   }
   getShowRows(){
     return this.rows;
   }
-  getPopulate(){
+
+  buildIndexQuery(){
+    var query = "db.collection('" + this.name + "')";
+    if(this.query){
+      query = query + ".find(" + this.query + ")";
+    }
+    else{
+      query = query + ".find()";
+    }
+    if(this.order == "desc")
+    {
+      return query + ".sort(-" + this.sortby + ")";
+    }
+    else
+    {
+      return query + ".sort(" + this.sortby + ")";
+    }
+    return query;
+  }
+
+  //Metodi della Classe
+
+  //Metodi Get
+  getPopulateIndex(){
      return this.indexPopulate;
    }
+
   getPopulateShow(){
     return this.populate;
   }
+  //Metodo Creazione Query di Show
+  buildShowQuery(id){
+    var query = "db.collection(" + this.name +
+    ").find({_id:" + id + "})";
+
+    return query;
+  }
+
+  //Metodo di specifica del tipo di DSL
   DSLType(){
     return "collection";
   }
 
-  buildQuery(){
-    var query = "db.collection('" + this.name + "')";
-    if(this.query)
-      query = query + ".find(" + this.query + ")";
-    else
-      query = query + ".find()";
-    if(this.order == "desc")
-      return query + ".sort(-" + this.sortby + ")";
-    else
-      return query + ".sort(" + this.sortby + ")";
-    return query;
-  }
-
-  buildShowQuery(id){
-    var query = "db.collection(" + this.name + ").find({_id:" + id + "})";
-    return query;
-  }
-
+  //Metodo per la costruzione del JSON risultante
   JSONbuild(result){
     return {
-      "properties":{"DSLType":this.DSLType(), "indexColumns":this.getIndexColumns(), "showRows":this.getShowRows(), "showPopulate":this.getPopulateShow()},
+      "properties":{"DSLType":this.DSLType(),
+      "indexColumns":this.getIndexColumns(),
+      "showRows":this.getShowRows(),
+      "showPopulate":this.getPopulateShow()},
       "data":{"result":result}
     };
   }
-
   render(store){
     var populate = this.getPopulate();
     if(this.flag){                                                              //EXECUTES ONCE
