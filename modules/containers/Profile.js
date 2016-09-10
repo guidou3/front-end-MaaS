e889e65a741122456536b5a67129352f95d1ade2
guidou3 <guido.santi@studenti.unipd.it>
2016-09-10 11:41:00 +0200

    Profile

--- modules/containers/Profile.js
+++ modules/containers/Profile.js
@@ -2,6 +2,7 @@ import React, { Component, PropTypes } from 'react'
 import * as actions from '../actions/RootAction'
 import MButton from '../components/MButton'
 import MTextBox from '../components/MTextBox'
+import { Button } from 'react-bootstrap'
 
 class Profile extends Component {
   constructor(props) {
@@ -18,24 +19,39 @@ class Profile extends Component {
     else {
         this.warn = ""
     }
+    let role =""
+    let n = store.getState().loggedUser.accessLevel
+    if(n==1) role = "Member"
+    else if (n==2) role = "Admin"
+    else if (n==3) role = "Owner"
 
     this.image = 'http://i.stack.imgur.com/HQwHI.jpg'
     return (
-
-  	  <div>
-        <h2>Profile</h2>
-        <p>{store.getState().loggedUser.accout}</p>
-
+      <div className="container">
         <div>
-          <img src={this.image} alt="Profile image" className="img-responsive"/>
+            <div>
+              <h1>Profile</h1>
+              <div className="form-group profile">
+                  <h4>Email: </h4>
+                  <MTextBox type="email" name="email" id="email" className="form-control" value= {store.getState().loggedUser.account} disabled="true"/>
+              </div>
+              <div className="form-group profile">
+                  <h4>Company: </h4>
+                  <MTextBox type="text" name="email" id="email" className="form-control" value= {store.getState().loggedUser.company} disabled="true"/>
+              </div>
+              <div className="form-group profile">
+                <h4>Role: </h4>
+                  <MTextBox type="text" name="email" id="email" className="form-control" value= {role} disabled="true"/>
+              </div>
+              <Button bsSize="large" bsStyle="primary"  onClick = {() => {
+                store.dispatch(actions.sendResetMail(store.getState().loggedUser.account))
+                store.dispatch(actions.redirect("/home"))
+              }}>
+                Change password
+              </Button>
+            </div>
+
         </div>
-        {this.warn}
-        <MButton label = "Change image" onClick = {() => {
-          store.dispatch(actions.refresh())
-        }}/>
-        <MButton label = "Change password" onClick = {() => {
-          store.dispatch(actions.redirect('/profile/changepwd'))
-        }}/>
       </div>
   	)
   }
